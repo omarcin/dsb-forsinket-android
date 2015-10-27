cb5182679788aadaf61ef77a51d5685e706d98ce
Marcin Oczeretko <marcin.oczeretko@gmail.com>
2015-10-28 00:27:21 +0100

    Different notfication icon for older versions

--- app/src/main/java/com/oczeretko/dsbforsinket/gcm/GcmMessageListenerService.java
+++ app/src/main/java/com/oczeretko/dsbforsinket/gcm/GcmMessageListenerService.java
@@ -35,7 +35,7 @@ public class GcmMessageListenerService extends GcmListenerService {
 
         Uri defaultSoundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
         NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(this)
-                                                             .setSmallIcon(R.drawable.ic_notification)
+                                                             .setSmallIcon(getNotificationIcon())
                                                              .setContentTitle(getString(R.string.app_name))
                                                              .setContentText(message)
                                                              .setAutoCancel(true)
@@ -46,5 +46,10 @@ public class GcmMessageListenerService extends GcmListenerService {
         NotificationManager notificationManager = (NotificationManager)getSystemService(Context.NOTIFICATION_SERVICE);
 
         notificationManager.notify(0 /* ID of notification */, notificationBuilder.build());
+    }
+
+    private int getNotificationIcon() {
+        boolean whiteIcon = (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.LOLLIPOP);
+        return whiteIcon ? R.drawable.ic_notification : R.mipmap.ic_launcher;
     }
 }
