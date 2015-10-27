3f19d0f977c722ae98d47d91b710eddc734ec1be
Marcin Oczeretko <marcin.oczeretko@gmail.com>
2015-10-28 00:27:31 +0100

    Use a different station for testing

--- app/src/main/java/com/oczeretko/dsbforsinket/Consts.java
+++ app/src/main/java/com/oczeretko/dsbforsinket/Consts.java
@@ -8,7 +8,7 @@ public final class Consts {
     public static final String SENT_TOKEN_TO_SERVER = "SENT_TOKEN";
     public static final String REGISTRATION_COMPLETE = "REG_COMPL";
 
-    public static final String STATION = "8600856";
+    public static final String STATION = "8600655";
 
     private Consts (){
     }
